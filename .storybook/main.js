c701885f5b019e5999df8afbe65ae45329fb1360
nakamura <larviodes@gmail.com>
2025-08-20 17:44:35 +0800

    Updates

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+const config = {
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-webpack5-compiler-swc", "@storybook/addon-docs", "@storybook/addon-onboarding"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+};
+export default config;
